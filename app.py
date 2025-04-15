30b78838de46c51d205d917db85830494ee75196
aimanalishezan <aimanalishezan22@gmail.com>
2025-04-15 19:24:32 +0600

    so much tired

--- app.py
+++ app.py
@@ -7,3 +7,14 @@ import google.generativeai
 from IPython.display import Markdown,display,update_display
 import gradio as gr
 import subprocess
+
+
+load_dotenv()
+openai=OpenAI
+OPENAI_MODEL="gpt-4o-mini"
+
+
+def write_output(code, file_name):
+    "Write the generated code to a file."
+    with open (file_name, "w") as f:
+        f.write(code)
